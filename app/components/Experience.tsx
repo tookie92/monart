f97c2842f16b9f2908d2622ed36c6bba352e7092
joseph ikinda <josephikinda@gmail.com>
2024-03-08 23:26:20 +0100

    last try bi

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -2,20 +2,10 @@ import { CameraControls } from '@react-three/drei'
 import React, { useEffect, useRef } from 'react'
 
 function Experience(props:any) {
-    const controls = useRef<any>()
-
-    const intro = async()=>{
-        controls.current.dolly(-22)
-        controls.current.smoothTime = 1.6
-        controls.current.dolly(22, true)
-    }
-
-    useEffect(()=>{
-        intro()
-    })
+    
   return (
     <>
-        <CameraControls ref={controls}/>
+      
         <ambientLight intensity={0.5}/>
         <directionalLight intensity={0.5} position={[0, 10, 0]} />
         <group {...props}>
