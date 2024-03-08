8cc203abd61259f9e0d7d3ea11d456491ccafc1f
joseph ikinda <josephikinda@gmail.com>
2024-03-08 22:36:46 +0100

    first commit

---
+++ app/components/Experience.tsx
@@ -0,0 +1,28 @@
+import { CameraControls } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react'
+
+function Experience() {
+    const controls = useRef<any>()
+
+    const intro = async()=>{
+        controls.current.dolly(-22)
+        controls.current.smoothTime = 1.6
+        controls.current.dolly(22, true)
+    }
+
+    useEffect(()=>{
+        intro()
+    })
+  return (
+    <>
+        <CameraControls ref={controls}/>
+        <ambientLight intensity={0.5}/>
+        <mesh>
+            <boxGeometry args={[3,5,2]}/>
+            <meshBasicMaterial color={"red"}/>
+        </mesh>
+    </>
+  )
+}
+
+export default Experience
---
+++ app/components/MonCanvas.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import Experience from './Experience'
+import { ARButton, Controllers, XR } from '@react-three/xr'
+
+function MonCanvas() {
+  return (
+    <>
+    <ARButton/>
+        <Canvas camera={{position:[0,0,8]}}>
+            <XR referenceSpace='local'>
+            <Controllers/>
+                <color attach={"background"} args={["#171720"]} />
+                <Experience/>
+            </XR>
+        </Canvas>
+    </>
+    
+  )
+}
+
+export default MonCanvas
